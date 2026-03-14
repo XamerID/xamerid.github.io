77214da449125ece86341341f38795494bbd530f
Xamer <xamerid@gmail.com>
2026-03-15 06:14:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,8 @@
     let current_Title = "media";
     
    
-  fetch(API + "/visit", {methods: "post",})
-    .then(response => console.log("Laporan terkirim ke Termux!"))
-    .catch(error => console.error("Server Termux mati atau link salah."));
+  fetch(API + "/visit", {
+  method: "POST",})
 
 
     function resetConvertUI() {
