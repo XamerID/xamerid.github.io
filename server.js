903291eb569f28f27f1ecd7a5d30556711c93afe
Xamer <xamerid@gmail.com>
2026-03-21 20:43:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@
     const previewImg = document.querySelector("#preview-img-converter");
 const hostIN = document.querySelector("#host-info");
 
-    const API = "https://antiques-plugins-spectacular-pins.trycloudflare.com";
+    const API = "https://supposed-pad-analysis-python.trycloudflare.com";
     let converting = false;
     let current_Title = "media";
     let current_process = null;
