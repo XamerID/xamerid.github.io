4862284a250631609dfe3ee8a645896088a0fe70
Xamer <xamerid@gmail.com>
2026-03-26 18:31:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
     const convertFm = document.querySelector("#converter-button-format");
     const previewImg = document.querySelector("#preview-img-converter");
 const hostIN = document.querySelector("#host-info");
-    const API = "https://door-part-ton-store.trycloudflare.com";
+    const API = "https://those-baking-rom-ministers.trycloudflare.com";
     let converting = false;
     let current_Title = "media";
     let current_process = null;
